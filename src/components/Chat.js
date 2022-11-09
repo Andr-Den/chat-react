e15f62ed1de89a9373a96cf9c99a31036ab7847a
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-11-09 21:50:45 +0500

    add component Chat

---
+++ src/components/Chat.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import MessageList from './MessageList';
+
+function Chat() {
+
+  return (
+    <div className="page">
+      <h2 className="chat__title">Комната А</h2>
+      <MessageList />
+      <form>
+        
+      </form>
+    </div>
+  )
+};
+
+export default Chat;

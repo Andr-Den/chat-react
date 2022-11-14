912a0b7a544573669aff8fd6319954ad45ed8299
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-11-14 14:16:26 +0500

    add component Logn

---
+++ src/components/Login.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Register.css'
+
+function Login({handleSubmit}) {
+
+  return (
+    <div className="page">
+      <div className="sign">
+        <form onSubmit={handleSubmit}>
+          <fieldset className="sign__container">
+            <h2 className="sign__title">Добро пожаловать!</h2>
+            <span className="sign__description">Имя</span>
+            <input type="text" className="sign__input"  required/>
+            <span className="sign__description">Пароль</span>
+            <input type="password" className="sign__input" required minLength="6"/>
+            <input type="submit" value='Войти' name="submit_button" className={`sign__button sign__button_login`}/>
+            <span className="sign__bottom">Ещё не зарегистрированы? <a href="/sign-up" className="sign__link">Регистрация</a></span>
+          </fieldset> 
+        </form>
+      </div>
+    </div>
+  )
+};
+
+export default Login;

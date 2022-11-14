32a26b71b860bce5bdb93c417b69eec6f2d3cfec
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-11-14 14:17:04 +0500

    add component InfoTooltip

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,14 +2,35 @@ import React from 'react'
 
 import MessageList from './MessageList';
 
+import './Chat.css'
+
 function Chat() {
+  const [messages, setMessages] = React.useState([])
+
+  const [messageText, setMessageText] = React.useState('');
+
+  function handleTextChange(e) {
+    setMessageText(e.target.value);
+  }
+
+  function handleMessageSubmit(e) {
+    e.preventDefault();
+    const userName = localStorage.getItem('user')
+    const message = {name: userName, text: messageText}
+    setMessages([...messages, message])
+  }
+
+  React.useEffect(() => {
+    console.log(JSON.parse(localStorage.getItem('usersList')))
+  }, [messages])
 
   return (
     <div className="page">
       <h2 className="chat__title">Комната А</h2>
-      <MessageList />
-      <form>
-        
+      {messages.length !== 0 ? <MessageList messages={messages}/>: ''}
+      <form onSubmit={handleMessageSubmit}>
+        <input type="text" onChange={handleTextChange} value={messageText || ''}/>
+        <input type="submit" value="Отправить" name="submit_button" className={`sign__button`} />
       </form>
     </div>
   )

3f6eb65feaa0724a7f478800e9541458e1f65930
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-11-15 11:57:20 +0500

    fix some bugs

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,8 +8,8 @@ function Main() {
   return (
     <div className="main">
       <div className="main__container">
-        <button className="main__button"><Link to="/sign-up" className="main__links">Зарегистрироваться</Link></button>
-        <button className="main__button"><Link to="/sign-in" className="main__links">Войти</Link></button>
+        <Link to="/sign-up" className="main__links">Зарегистрироваться</Link>
+        <Link to="/sign-in" className="main__links">Войти</Link>
       </div>
     </div>
   )
